d12dca9bd300d5eee48f5f216d68c00101f47bd0
Миркаримов Даурен Нурланович <@step.kz>
2023-11-23 21:35:38 +0600

    lesson_06 files

--- bboard/models.py
+++ bboard/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 
+is_all_posts_passive = False
+def is_active_default():
+    return is_all_posts_passive
 class Rubric(models.Model):
-    name = models.CharField(max_length=20, db_index=True, verbose_name="Название")
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Название", unique = True)
 
 
     def __str__(self):
@@ -14,12 +17,39 @@ class Rubric(models.Model):
 
 
 class Bb(models.Model):
+
+    # KINDS = (
+    #     ('Купля-продажа', (
+    #         ('b', 'Куплю'),
+    #         ('s', 'Продам'),
+    #     )),
+    #     ('Обмен', (
+    #     ('c', 'Обменяю'),
+    #     ))
+    # )
+
+    # KINDS = (
+    #     ('b', 'Куплю'),
+    #     ('s', 'Продам'),
+    #     ('c', 'Обменяю'),
+    # )
+    KINDS = (
+        (None, 'Выберите тип обьявления'),
+        ('b', 'Куплю'),
+        ('s', 'Продам'),
+        ('c', 'Обменяю'),
+    )
+
+    # kind = models.CharField(max_lenght=1, choices=KINDS, default='s')
+    kind = models.CharField(max_length=1, choices=KINDS, default='s')
     rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT, verbose_name="Рубрика")
     title = models.CharField(max_length=50, verbose_name="Товар")
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
-    price = models.FloatField(null=True, blank=True, verbose_name= "Цена")
+    price = models.DecimalField(max_digits=8, decimal_places = 2, verbose_name= "Цена", default=0.0)
+    is_active = models.BooleanField(default=is_active_default)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
-
+    email = models.SlugField(max_length =50, allow_unicode = True)
+    # id = models.UUIDField(primary_key=True, default = uuid.uuid4, editable=False)
     def __str__(self):
         return f'{self.title}'
 
