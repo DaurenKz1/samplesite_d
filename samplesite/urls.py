008ef879356e70dec30ad1144640bfa4aa049f1c
Миркаримов Даурен Нурланович <@step.kz>
2023-12-26 21:33:49 +0600

    lee

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', include('testapp.urls', namespace='test')),
     path('', include('bboard.urls', namespace='bboard')),
 ]
